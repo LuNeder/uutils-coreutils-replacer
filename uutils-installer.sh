9af7398db68d8126941eca5cd697b536f5d151c7
Luana <luana@luana.dev.br>
2023-09-30 15:26:47 -0300

    Update uutils-installer.sh

--- uutils-installer.sh
+++ uutils-installer.sh
@@ -12,9 +12,12 @@
 commandsuu="[ arch b2sum b3sum base32 base64 basename basenc cat chgrp chmod chown chroot cksum comm cp csplit cut date dd df dir dircolors dirname du echo env expand expr factor false fmt fold groups hashsum head hostid hostname id install join kill link ln logname ls md5sum mkdir mkfifo mknod mktemp more mv nice nl nohup nproc numfmt od paste pathchk pinky pr printenv printf ptx pwd readlink realpath relpath rm rmdir seq sha1sum sha224sum sha256sum sha3-224sum sha3-256sum sha3-384sum sha3-512sum sha384sum sha3sum sha512sum shake128sum shake256sum shred shuf sleep sort split stat stdbuf stty sum sync tac tail tee test timeout touch tr true truncate tsort tty uname unexpand uniq unlink uptime users vdir wc who whoami yes"
 
 coreutils echo "Run this first with sudo sh, then run 'sudo rpm -e --allmatches --nodeps coreutils', and then run this with sudo sh once again."
-coreutils echo "Only reboot AFTER running this after you remove coreutils with rpm. Remember to zypper addlock coreutils"
+coreutils echo "Only reboot AFTER running this after you remove coreutils with rpm."
+coreutils echo "Remember to install the empty .rpm coreutils metapackage with zypper and then addlock coreutils and coreutils-single on zypper."
 
 for i in $commandsuu; do
     coreutils rm -f /usr/bin/$i
     coreutils ln -s /usr/bin/coreutils /usr/bin/$i
 done
+
+echo done
