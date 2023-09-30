452bbe119e6d3c9b6ebff059da935206b761a70f
Luana <luana@luana.dev.br>
2023-09-30 16:42:07 -0300

    Create zsh-completions.sh

---
+++ zsh-completions.sh
@@ -0,0 +1,6 @@
+commandsuu="[ arch b2sum b3sum base32 base64 basename basenc cat chgrp chmod chown chroot cksum comm cp csplit cut date dd df dir dircolors dirname du echo env expand expr factor false fmt fold groups hashsum head hostid hostname id install join kill link ln logname ls md5sum mkdir mkfifo mknod mktemp more mv nice nl nohup nproc numfmt od paste pathchk pinky pr printenv printf ptx pwd readlink realpath relpath rm rmdir seq sha1sum sha224sum sha256sum sha3-224sum sha3-256sum sha3-384sum sha3-512sum sha384sum sha3sum sha512sum shake128sum shake256sum shred shuf sleep sort split stat stdbuf stty sum sync tac tail tee test timeout touch tr true truncate tsort tty uname unexpand uniq unlink uptime users vdir wc who whoami yes"
+
+for i in $commandsuu; do
+    cargo run completion $i bash > /usr/share/bash-completion/completions/$i
+    cargo run completion $i zsh > /usr/share/zsh/site-functions/_$i
+done
